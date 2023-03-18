dd023f6a53e36f343da3e249bdffbb4b76282c7c
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-18 20:20:16 +0200

    Update index.jsx

--- pages/pokemons/index.jsx
+++ pages/pokemons/index.jsx
@@ -1,22 +1,58 @@
+import About from "@/components/About";
+import HomeProvider, { HomeContext } from "@/components/Context";
+import { fetchPokemon } from "@/helpers/fetchPokemon";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import { useQuery } from "react-query";
 
 const HomePage = () => {
-  useEffect(() => {
-    const getTransactions = async () => {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
-      const data = await response.json();
-      console.log("getTransactions  data:", data);
-    };
-    getTransactions();
-  }, []);
+  const { count, setCount, isShow, setIsShow } = useContext(HomeContext);
+
+  const { isLoading, data } = useQuery("pokemon", () =>
+    fetchPokemon("bulbasaur")
+  );
+
+  // const incrementCount = () => setCount((p) => p + 1);
+  // const decrementCount = () => setCount((p) => p - 1);
+
+  const toggleVisible = () => setIsShow((p) => !p);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
       <h1>Home Page</h1>
+
       <Link href="/">HOME</Link>
+      <Link href="/pokemons/second">SECOND</Link>
+
+      {isShow && <About />}
+      <button type="button" onClick={toggleVisible}>{isShow ? "Скрыть" : "Показать"}</button>
+
+
+      <ul>
+        {data.results.map((item) => {
+          return <li key={item.name}>{item.name}</li>;
+        })}
+      </ul>
+
+      {/* <h2>{count}</h2>
+      <button type="button" onClick={incrementCount}>Добавить</button>
+      <button type="button" onClick={decrementCount}>Отнять</button> */}
+
+      
     </>
   );
 };
 
 export default HomePage;
+
+// const WrapperContext = () => (
+//   <HomeProvider>
+//     <HomePage />
+//   </HomeProvider>
+// );
+
+// export default WrapperContext;
