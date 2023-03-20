437eb80f7c2de9956725ba5dbecc0c100c8c91a9
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-20 13:17:10 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import "@/styles/globals.css";
+import  { useState } from "react";
 import HomeProvider from "@/components/Context";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+// import { ReactQueryDevtools } from "react-query/devtools";
+// import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider, Hydrate } from "@tanstack/react-query";
 
 const App = ({ Component, pageProps }) => {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <>
       <HomeProvider>
         <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-          <ReactQueryDevtools />
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+          {/* <ReactQueryDevtools /> */}
         </QueryClientProvider>
       </HomeProvider>
     </>
