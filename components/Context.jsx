106c8de881cd450d5c87722475261027ed694a50
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-21 22:12:52 +0200

    Update Context.jsx

--- components/Context.jsx
+++ components/Context.jsx
@@ -1,4 +1,5 @@
 import { fetchPokemon } from "@/helpers/fetchPokemon";
+import { getAllTransactions } from "@/helpers/getAllTransactions";
 import { useQuery } from "@tanstack/react-query";
 import { createContext, useEffect, useState } from "react";
 
@@ -8,8 +9,23 @@ const HomeProvider = ({ children }) => {
   const [count, setCount] = useState(0);
   const [isShow, setIsShow] = useState(false);
 
+
+  const [ddd, setDdd] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [pageNum, setPageNum] = useState("");
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["pokemon", pageNum],
+    queryFn: () => getAllTransactions(pageNum),
+    enabled: pageNum.length > 0,
+  });
+
+  useEffect(() => {
+    if (data) setDdd((prev) => [...prev, ...data]);
+  }, [data]);
+
   return (
-    <HomeContext.Provider value={{ count, setCount, isShow, setIsShow }}>
+    <HomeContext.Provider value={{ddd, setPageNum, searchValue, setSearchValue, count, setCount, isShow, setIsShow }}>
       {children}
     </HomeContext.Provider>
   );
