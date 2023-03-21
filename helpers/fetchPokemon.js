160f27c0e93ac5361541beace70dbb0084b3fd36
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-21 22:12:55 +0200

    Update fetchPokemon.js

--- helpers/fetchPokemon.js
+++ helpers/fetchPokemon.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchPokemon = async (name) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const data = await response.json();
   // const { data } = await axios(`https://pokeapi.co/api/v2/pokemon/${name}`);
   return data.name;
