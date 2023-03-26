bd937b733069f1c5e86bdd2b466332de1e410a88
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-26 21:46:06 +0300

    Update getAllTransactions.js

--- helpers/getAllTransactions.js
+++ helpers/getAllTransactions.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3OTA3MjM1NCwiZXhwIjoxNjgwMjgxOTU0fQ.u1VWvbA1LAtnjGSqFXWolUmnIidmrquomyDn4H9KmUU`;
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-export const getAllTransactions = async (pageNum) => {
-  const {data}  = await axios(
+export const getAllTransactions = async (pageNum = 1) => {
+  const { data } = await axios(
     `https://wallet-backend-xmk0.onrender.com/api/transactions?page=${pageNum}&limit=5`
   );
-//   console.log("getAllTransactions  data.transactions:", data);
+
   return data.transactions;
 };
