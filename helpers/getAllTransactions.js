3b1c346621810b5b82d4da901c6dd1e41e717ad6
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-04-01 21:54:22 +0300

    Update getAllTransactions.js

--- helpers/getAllTransactions.js
+++ helpers/getAllTransactions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3OTA3MjM1NCwiZXhwIjoxNjgwMjgxOTU0fQ.u1VWvbA1LAtnjGSqFXWolUmnIidmrquomyDn4H9KmUU`;
+const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3OTU3NzcxMiwiZXhwIjoxNjgwNzg3MzEyfQ.VY_G-L1q8yYS7Q_c_l3Y4bsNAYG3wuAH2wrp-kXBNBY`;
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
 export const getAllTransactions = async (pageNum = 1) => {
