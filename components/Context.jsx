d07409bf6f3741f4d156917c5d01bad4b36e217d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-04-04 20:10:58 +0300

    Update Context.jsx

--- components/Context.jsx
+++ components/Context.jsx
@@ -1,6 +1,6 @@
 import { fetchPokemon } from "@/helpers/fetchPokemon";
 import { getAllTransactions } from "@/helpers/getAllTransactions";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { createContext, useEffect, useRef, useState } from "react";
 
 export const HomeContext = createContext();
@@ -8,24 +8,28 @@ export const HomeContext = createContext();
 const ContextProvider = ({ children }) => {
   // const [count, setCount] = useState(0);
   // const [isShow, setIsShow] = useState(false);
+  // const firstRender = useRef(true);
 
   const [pageNum, setPageNum] = useState(1);
   const [transactions, setTransactions] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 
-  const firstRender = useRef(true);
 
-  const { isLoading, data } = useQuery({
-    queryKey: ["transactions", pageNum],
-    queryFn: () => getAllTransactions(pageNum),
-    // refetchOnWindowFocus: false,
-    // enabled: false,
-    onSuccess: (data) => {
-      setTransactions((prev) => [...prev, ...data]);
-    },
-  });
-  console.log("ContextProvider  data:", data);
+  // const queryClient = useQueryClient();
+  // console.log("ContextProvider", queryClient.getQueryData());
+
+  // const { isLoading, data } = useQuery({
+  //   queryKey: ["transactions", pageNum],
+  //   queryFn: () => getAllTransactions(pageNum),
+  //   // refetchOnWindowFocus: false,
+  //   // enabled: false,
+  //   onSuccess: (data) => {
+  //     // setTransactions((prev) => [...prev, ...data]);
+  //     setTransactions(data);
+  //   },
+  // });
+  
 
 
   // useEffect(() => {
