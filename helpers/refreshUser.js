5b59798d7596ef7ef176c142bea7a5dbc6267841
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-04-05 21:58:47 +0300

    Update refreshUser.js

--- helpers/refreshUser.js
+++ helpers/refreshUser.js
@@ -1,13 +1,20 @@
 import axios from "axios";
+import { parseCookies } from "nookies";
+import { authHeader } from "./setToken";
 
 const BASE_URL = "https://wallet-backend-xmk0.onrender.com/api";
 const USER_CURRENT = "/users/current";
 
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3OTU3NzcxMiwiZXhwIjoxNjgwNzg3MzEyfQ.VY_G-L1q8yYS7Q_c_l3Y4bsNAYG3wuAH2wrp-kXBNBY`;
-
 export const refreshUser = async () => {
+  const { authToken } = parseCookies();
+
+  // if (!authToken) {
+  //   return
+  // }
+
+  authHeader.set(authToken);
+
   const { data } = await axios.get(`${BASE_URL}${USER_CURRENT}`);
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
   return data;
 };
