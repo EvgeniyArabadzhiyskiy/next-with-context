103d323333130ed3ecea7e820021f7bbf6ee446f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-04-14 22:43:25 +0300

    Update createTransaction.js

--- helpers/createTransaction.js
+++ helpers/createTransaction.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const TRANSACTIONS = "/transactions";
 const BASE_URL = "https://wallet-backend-xmk0.onrender.com/api";
 
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3OTU3NzcxMiwiZXhwIjoxNjgwNzg3MzEyfQ.VY_G-L1q8yYS7Q_c_l3Y4bsNAYG3wuAH2wrp-kXBNBY`;
-axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+// const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3OTU3NzcxMiwiZXhwIjoxNjgwNzg3MzEyfQ.VY_G-L1q8yYS7Q_c_l3Y4bsNAYG3wuAH2wrp-kXBNBY`;
+// axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
 export const createTransaction = async (transaction) => {
   const {data} = await axios.post(`${BASE_URL}${TRANSACTIONS}`, transaction);
-  console.log("SecondPage  data:", data);
+  // console.log("SecondPage  data:", data);
   return data;
 };
