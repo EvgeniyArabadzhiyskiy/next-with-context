dcd1aabceb5175bcdc50801800f88372f9493152
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-04-30 21:30:11 +0300

    Create [pageNum].jsx

---
+++ pages/transactions/no-query/[pageNum].jsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { getAllTransactions } from "@/helpers/getAllTransactions";
+
+export async function getServerSideProps(context) {
+  const pageNum = Number(context.params.pageNum);
+  const todos = await getAllTransactions(pageNum);
+    
+  return {
+    props: { todos },
+  };
+}
+
+export default function TodosPage({ todos }) {
+  const router = useRouter();
+  const pageNum = Number(router.query.pageNum) || 1;
+
+  const handleNextClick = () => {
+    const currentPageNum = Number(pageNum);
+    const nextPageNum = currentPageNum + 1;
+    router.push(`/transactions/new/${nextPageNum}`);
+  };
+
+  const handlePrevClick = () => {
+    const currentPageNum = Number(pageNum);
+    const nextPageNum = currentPageNum - 1;
+    router.push(`/transactions/new/${nextPageNum}`);
+  };
+
+  return (
+    <>
+      <Link href="/">HOME</Link>
+      <button type="button" onClick={handleNextClick}>Next Page</button>
+      <button type="button" disabled={pageNum <= 1} onClick={handlePrevClick}>Prev Page</button>
+
+    <h1>Server Prop no Query</h1>
+      <ul>
+        {todos?.map((item) => {
+          return (
+          <div style={{display: 'flex', justifyContent: 'space-between', width: '200px'}} key={item._id}>
+            <li>{item.category}</li>
+          </div>)
+        })}
+      </ul>
+    </>
+  );
+}
