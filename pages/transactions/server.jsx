e9957da2f4b60893a8d2a411b18ac7b2a0116d62
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-04-30 13:13:30 +0300

    Update server.jsx

--- pages/transactions/server.jsx
+++ pages/transactions/server.jsx
@@ -5,16 +5,16 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 
-export async function getServerSideProps(context) {
-  const queryClient = new QueryClient();
-  // const pageNum = Number(context.params.pageNum);
+// export async function getServerSideProps(context) {
+//   const queryClient = new QueryClient();
+//   // const pageNum = Number(context.params.pageNum);
 
-  await queryClient.prefetchQuery(["transactions", 1], () => getAllTransactions(1));
+//   await queryClient.prefetchQuery(["transactions", 1], () => getAllTransactions(1));
 
-  return {
-    props: { dehydratedState: dehydrate(queryClient) },
-  };
-}
+//   return {
+//     props: { dehydratedState: dehydrate(queryClient) },
+//   };
+// }
 
 // export async function getServerSideProps(context) {
 //   const pageNum = context.params.pageNum;
@@ -26,101 +26,51 @@ export async function getServerSideProps(context) {
 // }
 
 
-export default function TodosPage({ todos }) {
+// export default function TodosPage({ todos }) {
 
-  const queryClient = useQueryClient()
-  // const [isSkip, setIsSkip] = useState(false);
+//   const queryClient = useQueryClient()
+//   // const [isSkip, setIsSkip] = useState(false);
  
   
-  const [pageNum, setPageNum] = useState(1);
-  // console.log("TodosPage  pageNum:", pageNum);
+//   // const [pageNum, setPageNum] = useState(1);
+//   // console.log("TodosPage  pageNum:", pageNum);
 
-  // const [page, setPage] = useState(todos);
+//   // const [page, setPage] = useState(todos);
   
 
-  // const router = useRouter();
-  // const pageNum = router.query.pageNum || 1;
-
-  const { data } = useQuery({
-    queryKey: ["transactions", pageNum],
-    queryFn: () => getAllTransactions(pageNum),
-    // initialData: todos,
-    // enabled: isSkip,
-
-    // onSuccess:(data) => setPage(data),
-
-    keepPreviousData: true,
-    staleTime: Infinity,
-  });
-
-  // const queryLength = queryClient.getQueriesData(["transactions"]);
-  // console.log("TodosPage  queryLength:", queryLength);
-
-  const handleNextClick = () => {
-    setPageNum(p => p + 1)
-
-    // const currentPageNum = Number(pageNum);
-    // const nextPageNum = currentPageNum + 1;
-    // router.push(`/transactions/${nextPageNum}`);
-  };
-
-  const handlePrevClick = () => {
-    setPageNum(p => p - 1);
-    // setIsSkip(true)
-    
-    // const currentPageNum = Number(pageNum);
-    // const nextPageNum = currentPageNum - 1;
-    // router.push(`/transactions/${nextPageNum}`);
-  };
-
-  return (
-    <>
-      <Link href="/">HOME</Link>
-      <button type="button" onClick={handleNextClick}>Next Page</button>
-      <button type="button" disabled={pageNum <= 1} onClick={handlePrevClick}>Prev Page</button>
-
-      <ul>
-        {data?.map((item) => {
-          return (
-          <div style={{display: 'flex', justifyContent: 'space-between', width: '200px'}} key={item._id}>
-            <li>{item.category}</li>
-          </div>)
-        })}
-      </ul>
-    </>
-  );
-}
-
-//============================================================
-// export default function TodosPage() {
 //   const router = useRouter();
-//   const [pageNum, setPageNum] = useState(1);
-  
-//   useEffect(() => {
-//     const { pageNum } = router.query;
-//     if (pageNum) {
-//       setPageNum(parseInt(pageNum));
-//     }
-//   }, [router.query]);
-  
+//   const pageNum = router.query.pageNum || 1;
+
 //   const { data } = useQuery({
 //     queryKey: ["transactions", pageNum],
 //     queryFn: () => getAllTransactions(pageNum),
+//     initialData: todos,
+//     // enabled: isSkip,
+
+//     // onSuccess:(data) => setPage(data),
 
 //     keepPreviousData: true,
 //     staleTime: Infinity,
 //   });
 
+//   // const queryLength = queryClient.getQueriesData(["transactions"]);
+//   // console.log("TodosPage  queryLength:", queryLength);
+
 //   const handleNextClick = () => {
+//     // setPageNum(p => p + 1)
+
 //     const currentPageNum = Number(pageNum);
 //     const nextPageNum = currentPageNum + 1;
-//     router.push(`/transactions/server?pageNum=${nextPageNum}`); 
+//     router.push(`/transactions/${nextPageNum}`);
 //   };
 
 //   const handlePrevClick = () => {
+//     // setPageNum(p => p - 1);
+//     // setIsSkip(true)
+    
 //     const currentPageNum = Number(pageNum);
-//     const prevPageNum = currentPageNum - 1;
-//     router.push(`/transactions/server?pageNum=${prevPageNum}`);
+//     const nextPageNum = currentPageNum - 1;
+//     router.push(`/transactions/${nextPageNum}`);
 //   };
 
 //   return (
@@ -144,7 +94,69 @@ export default function TodosPage({ todos }) {
 
 
 
-//============================================================
+
+//=======Только первая страница загружается на сервере ==========
+
+// export async function getServerSideProps(context) {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery(["transactions", 1], () => getAllTransactions(1));
+
+//   return {
+//     props: { dehydratedState: dehydrate(queryClient) },
+//   };
+// }
+
+// export default function TodosPage({ todos }) {
+//   const [pageNum, setPageNum] = useState(1);
+  
+//   const { data } = useQuery({
+//     queryKey: ["transactions", pageNum],
+//     queryFn: () => getAllTransactions(pageNum),
+  
+//     keepPreviousData: true,
+//     staleTime: Infinity,
+//   });
+
+//   const handleNextClick = () => {
+//     setPageNum(p => p + 1)
+//   };
+
+//   const handlePrevClick = () => {
+//     setPageNum(p => p - 1);
+
+//   };
+
+//   return (
+//     <>
+//       <Link href="/">HOME</Link>
+//       <button type="button" onClick={handleNextClick}>Next Page</button>
+//       <button type="button" disabled={pageNum <= 1} onClick={handlePrevClick}>Prev Page</button>
+
+//       <ul>
+//         {data?.map((item) => {
+//           return (
+//           <div style={{display: 'flex', justifyContent: 'space-between', width: '200px'}} key={item._id}>
+//             <li>{item.category}</li>
+//           </div>)
+//         })}
+//       </ul>
+//     </>
+//   );
+// }
+
+//====== Работает с initialData каждая страница загружается на сервере ===
+//============== динамический маршрут [pageNum] ======================
+
+// export async function getServerSideProps(context) {
+//   const pageNum = context.params.pageNum;
+//   const todos = await getAllTransactions(pageNum);
+  
+//   return {
+//     props: { todos },
+//   };
+// }
+
+
 // export default function TodosPage({ todos }) {
 //   const router = useRouter();
 //   const pageNum = router.query.pageNum || 1;
@@ -153,10 +165,11 @@ export default function TodosPage({ todos }) {
 //     queryKey: ["transactions", pageNum],
 //     queryFn: () => getAllTransactions(pageNum),
 //     initialData: todos,
-
+ 
 //     keepPreviousData: true,
 //     staleTime: Infinity,
 //   });
+
 
 //   const handleNextClick = () => {
 //     const currentPageNum = Number(pageNum);
@@ -189,56 +202,59 @@ export default function TodosPage({ todos }) {
 // }
 
 
-// export async function getServerSideProps() {
-//   const todos = await getAllTransactions(1);
-  
+//=========== getStaticPaths with getStaticProps ====================
+
+// export async function getStaticPaths ()  {
+//   console.log("HELLO get Static Paths");
+//   const allPagesNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 //   return {
-//     props: { todos },
-//   };
+//     paths: allPagesNum.map((p) => `/transactions/${p}`),
+//     fallback: false,
+//   }
 // }
 
-//============================================================
-
-// export default function TodosPage({ todos }) {
-//   const queryClient = useQueryClient()
-//   const [pageNum, setPageNum] = useState(1);
+export async function getStaticProps(context) {
+  console.log("Static Props");
+  // const pageNum = context.params.pageNum;
+  const todos = await getAllTransactions(1);
   
-//   const { data } = useQuery({
-//     queryKey: ["transactions", pageNum],
-//     queryFn: () => getAllTransactions(pageNum),
-  
-//     keepPreviousData: true,
-//     staleTime: Infinity,
-//   });
-
-//   // const queryLength = queryClient.getQueriesData(["transactions"]);
-//   // console.log("TodosPage  queryLength:", queryLength);
-
-//   const handleNextClick = () => {
-//     setPageNum(p => p + 1)
-//   };
-
-//   const handlePrevClick = () => {
-//     setPageNum(p => p - 1);
-
-//   };
-
-//   return (
-//     <>
-//       <Link href="/">HOME</Link>
-//       <button type="button" onClick={handleNextClick}>Next Page</button>
-//       <button type="button" disabled={pageNum <= 1} onClick={handlePrevClick}>Prev Page</button>
-
-//       <ul>
-//         {data?.map((item) => {
-//           return (
-//           <div style={{display: 'flex', justifyContent: 'space-between', width: '200px'}} key={item._id}>
-//             <li>{item.category}</li>
-//           </div>)
-//         })}
-//       </ul>
-//     </>
-//   );
-// }
+  return {
+    props: { todos },
+  };
+}
 
 
+export default function TodosPage({ todos }) {
+  // const router = useRouter();
+  // const pageNum = router.query.pageNum || 1;
+
+  // const handleNextClick = () => {
+  //   const currentPageNum = Number(pageNum);
+  //   const nextPageNum = currentPageNum + 1;
+  //   router.push(`/transactions/${nextPageNum}`);
+  // };
+
+  // const handlePrevClick = () => {
+  //   const currentPageNum = Number(pageNum);
+  //   const nextPageNum = currentPageNum - 1;
+  //   router.push(`/transactions/${nextPageNum}`);
+  // };
+
+  return (
+    <>
+      <Link href="/">HOME</Link>
+      {/* <button type="button" onClick={handleNextClick}>Next Page</button>
+      <button type="button" disabled={pageNum <= 1} onClick={handlePrevClick}>Prev Page</button> */}
+
+      <h1>Static Props</h1>
+      <ul>
+        {todos?.map((item) => {
+          return (
+          <div style={{display: 'flex', justifyContent: 'space-between', width: '200px'}} key={item._id}>
+            <li>{item.category}</li>
+          </div>)
+        })}
+      </ul>
+    </>
+  );
+}
