a62eb9d1bf0f56ff8edb7e4ecd667dcd7a3682e3
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-04-30 13:13:32 +0300

    Create [pageNum].jsx

---
+++ pages/transactions/new/[pageNum].jsx
@@ -0,0 +1,58 @@
+import { getAllTransactions } from "@/helpers/getAllTransactions";
+import { dehydrate, QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export async function getServerSideProps(context) {
+    const pageNum = context.params.pageNum;
+    const todos = await getAllTransactions(pageNum);
+    
+    return {
+      props: { todos },
+    };
+  }
+  
+  
+  export default function TodosPage({ todos }) {
+    const router = useRouter();
+    const pageNum = router.query.pageNum || 1;
+  
+    const { data } = useQuery({
+      queryKey: ["transactions", pageNum],
+      queryFn: () => getAllTransactions(pageNum),
+      initialData: todos,
+   
+      keepPreviousData: true,
+      staleTime: Infinity,
+    });
+  
+  
+    const handleNextClick = () => {
+      const currentPageNum = Number(pageNum);
+      const nextPageNum = currentPageNum + 1;
+      router.push(`/transactions/new/${nextPageNum}`);
+    };
+  
+    const handlePrevClick = () => {
+      const currentPageNum = Number(pageNum);
+      const nextPageNum = currentPageNum - 1;
+      router.push(`/transactions/new/${nextPageNum}`);
+    };
+  
+    return (
+      <>
+        <Link href="/">HOME</Link>
+        <button type="button" onClick={handleNextClick}>Next Page</button>
+        <button type="button" disabled={pageNum <= 1} onClick={handlePrevClick}>Prev Page</button>
+  
+        <ul>
+          {data?.map((item) => {
+            return (
+            <div style={{display: 'flex', justifyContent: 'space-between', width: '200px'}} key={item._id}>
+              <li>{item.category}</li>
+            </div>)
+          })}
+        </ul>
+      </>
+    );
+  }
