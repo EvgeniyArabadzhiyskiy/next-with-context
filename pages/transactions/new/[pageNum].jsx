7b962444e77ca32e49325401ac1cfa619c205f43
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-02 17:54:50 +0300

    Update [pageNum].jsx

--- pages/transactions/new/[pageNum].jsx
+++ pages/transactions/new/[pageNum].jsx
@@ -2,7 +2,6 @@ import { getAllTransactions } from "@/helpers/getAllTransactions";
 import { dehydrate, QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
 
 // export async function getServerSideProps(context) {
 //   const pageNum = Number(context.params.pageNum);
@@ -14,31 +13,25 @@ import { useState } from "react";
 // }
 
 
-
 export async function getServerSideProps(context) {
   const pageNum = Number(context.params.pageNum);
   const queryClient = new QueryClient();
+
   await queryClient.prefetchQuery(["transactions", pageNum], () =>
     getAllTransactions(pageNum)
   );
-
-  console.log("HELLO");
-
-  // const queryLength = queryClient.getQueriesData(["transactions"]);
-  // console.log("TodosPage  queryLength:", queryLength.length);
 
   return {
     props: { dehydratedState: dehydrate(queryClient) },
   };
 }
-  
-  
-export default function TodosPage({ todos }) {
-  const queryClient = useQueryClient();
 
-  // const [page, setPage] = useState(1);
-  // console.log("TodosPage  page:", page);
-  
+
+
+
+export default function TodosPage({ todos }) {
+  const queryClient = useQueryClient(); 
+
   const router = useRouter();
   const pageNum = Number(router.query.pageNum) || 1;
 
@@ -49,30 +42,21 @@ export default function TodosPage({ todos }) {
   
     keepPreviousData: true,
     staleTime: Infinity,
-
-    onSuccess: () => {
-      console.log('Hello');
-    }
   });
 
   const queryLength = queryClient.getQueriesData(["transactions"]);
   console.log("TodosPage  queryLength:", queryLength);
 
-
   const handleNextClick = () => {
     const currentPageNum = Number(pageNum);
     const nextPageNum = currentPageNum + 1;
     router.push(`/transactions/new/${nextPageNum}`);
-
-    // setPage((p) => p + 1);
   };
 
   const handlePrevClick = () => {
     const currentPageNum = Number(pageNum);
     const nextPageNum = currentPageNum - 1;
     router.push(`/transactions/new/${nextPageNum}`);
-
-    // setPage((p) => p - 1);
   };
 
   return (
